39e8a2f5a6ae2a4024c1602d05e9e091c4e59b28
Shubham Lagwankar <shubhu105@gmail.com>
2017-07-14 21:05:00 -0400

    percolation: Ignore backwash, fix style, bugs.

--- percolation/Percolation.java
+++ percolation/Percolation.java
@@ -1,9 +1,6 @@
-import java.lang.IllegalArgumentException;
-
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-public class Percolation
-{
+public class Percolation {
     private WeightedQuickUnionUF unionFind;
     private boolean[][] grid;
     private int n;
@@ -14,21 +11,20 @@ public class Percolation
     public Percolation(int n) {
         if (n <= 0) throw new IllegalArgumentException();
 
-        int N = n + 1;
-
-        unionFind = new WeightedQuickUnionUF(N * N);
-        grid = new boolean[N][N];
+        unionFind = new WeightedQuickUnionUF(n * n + 2);
+        grid = new boolean[n][n];
         this.n = n;
+        virtualTop = n * n;
+        virtualBottom = virtualTop + 1;
 
-        virtualTop = 0;
-        virtualBottom = n * n;
-
-        for (int col = 1; col <= n; col++)
+        for (int col = 1; col <= n; col++) {
             unionFind.union(xyTo1D(1, col), virtualTop);
+            unionFind.union(xyTo1D(n, col), virtualBottom);
+        }
     }
 
     private int xyTo1D(int x, int y) {
-         return x*n + y;
+        return (x - 1) * n + (y - 1);
     }
 
     private void validate(int row, int col) {
@@ -40,28 +36,29 @@ public class Percolation
         validate(row, col);
         if (isOpen(row, col)) return;
 
-        grid[row][col] = true;
+        grid[row - 1][col - 1] = true;
         openSites++;
 
-        int xy = xyTo1D(row, col);
-        boolean connectBottom = false;
-        if (row > 1 && isOpen(row - 1, col))
-            unionFind.union(xy, xyTo1D(row - 1, col));
+        int p = xyTo1D(row, col);
+        if (row > 1 && isOpen(row - 1, col)) {
+            // if (row == n && isFull(row - 1, col))
+            //     unionFind.union(p, virtualBottom);
+            unionFind.union(p, xyTo1D(row - 1, col));
+        }
         if (row < n && isOpen(row + 1, col)) {
-            if (row == n - 1) connectBottom = true;
-            unionFind.union(xy, xyTo1D(row + 1, col));
+            // if (row + 1 == n && isFull(row, col))
+            //     unionFind.union(xyTo1D(row + 1, col), virtualBottom);
+            unionFind.union(p, xyTo1D(row + 1, col));
         }
         if (col > 1 && isOpen(row, col - 1))
-            unionFind.union(xy, xyTo1D(row, col - 1));
+            unionFind.union(p, xyTo1D(row, col - 1));
         if (col < n && isOpen(row, col + 1))
-            unionFind.union(xy, xyTo1D(row, col + 1));
-        if (connectBottom && isFull(row, col))
-            unionFind.union(xyTo1D(row + 1, col), virtualBottom);
+            unionFind.union(p, xyTo1D(row, col + 1));
     }
 
     public boolean isOpen(int row, int col) {
         validate(row, col);
-        return grid[row][col];
+        return grid[row - 1][col - 1];
     }
 
     public boolean isFull(int row, int col) {
@@ -75,10 +72,9 @@ public class Percolation
     }
 
     public boolean percolates() {
+        if (n == 1 && isOpen(1, 1)) return true;
         return unionFind.connected(virtualTop, virtualBottom);
     }
 
-    public static void main(String[] args) {
-
-    }
+    public static void main(String[] args) { }
 }
