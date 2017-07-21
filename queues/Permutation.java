ebc6e5810d74267b692464af59665178ab545c1a
Shubham Lagwankar <shubhu105@gmail.com>
2017-07-21 07:28:51 -0400

    queues: Permutation client.

---
+++ queues/Permutation.java
@@ -0,0 +1,29 @@
+import java.util.NoSuchElementException;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Permutation {
+    public static void main(String[] args) {
+        int k = 0;
+
+        if (args.length == 1) k = Integer.parseInt(args[0]);
+        else                  return;
+
+        RandomizedQueue<String> rq = new RandomizedQueue<>();
+
+        try {
+            while (true) {
+                String s = StdIn.readString();
+                rq.enqueue(s);
+            }
+        } catch (NoSuchElementException e) {
+            if (rq.isEmpty()) return;
+        }
+
+        for (String item : rq) {
+            StdOut.println(item);
+            if (--k == 0) break;
+        }
+    }
+}
