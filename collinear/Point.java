78452d2c4c621116e508577ea79d9e2ace7f2996
Shubham Lagwankar <shubhu105@gmail.com>
2017-07-27 22:39:18 -0400

    collinear: Point, LineSegment.

--- collinear/Point.java
+++ collinear/Point.java
@@ -1,30 +1,55 @@
+import java.util.Comparator;
+import edu.princeton.cs.algs4.StdDraw;
+
 public class Point implements Comparable<Point> {
+    private final int x;
+    private final int y;
     
     public Point(int x, int y) {
-
+        this.x = x;
+        this.y = y;
     }
 
     public void draw() {
-
+        StdDraw.point(x, y);
     }
 
     public void drawTo(Point that) {
-
+        StdDraw.line(this.x, this.y, that.x, that.y);
     }
 
     public String toString() {
-
+        return "(" + x + ", " + y + ")";
     }
 
     public int compareTo(Point that) {
-
+        if (this.y > that.y) return 1;
+        if (this.y < that.y) return -1;
+        if (this.x > that.x) return 1;
+        if (this.x < that.x) return -1;
+        return 0;
     }
 
     public double slopeTo(Point that) {
-
+        if (this.compareTo(that) == 0) return Double.NEGATIVE_INFINITY;
+        if (this.x == that.x)          return Double.POSITIVE_INFINITY;
+        return (double) (that.y - this.y) / (that.x - this.x);
     }
 
     public Comparator<Point> slopeOrder() {
-
+        return new SlopeOrder();
     }
+
+    private class SlopeOrder implements Comparator<Point> {
+        public int compare(Point p, Point q) {
+            Point thisPoint = Point.this;
+            double diff = thisPoint.slopeTo(p) - thisPoint.slopeTo(q);
+
+            if (diff > 0.05) return 1;
+            if (diff < 0.05) return -1;
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) { }
 }
