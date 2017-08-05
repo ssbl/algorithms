d452719edc16bc5b3473ccc992cbfe7232b744d4
Shubham Lagwankar <shubhu105@gmail.com>
2017-08-05 14:49:01 -0400

    8puzzle: Skeleton.

---
+++ 8puzzle/Board.java
@@ -0,0 +1,43 @@
+public class Board {
+
+    public Board(int[][] blocks) {
+
+    }
+
+    public int dimension() {
+
+    }
+
+    public int hamming() {
+
+    }
+
+    public int manhattan() {
+
+    }
+
+    public boolean isGoal() {
+
+    }
+
+    public Board twin() {
+
+    }
+
+    public boolean equals(Object y) {
+
+    }
+
+    public Iterable<Board> neighbors() {
+
+    }
+
+    public String toString() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ 8puzzle/Solver.java
@@ -0,0 +1,23 @@
+public class Solver() {
+
+    public Solver(Board initial) {
+
+    }
+
+    public boolean isSolvable() {
+
+    }
+
+    public int moves() {
+
+    }
+
+    public Iterable<Board> solution() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
