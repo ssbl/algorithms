2f2d894fb5aeaa1374bfaad29b63db903c6f0884
Shubham Lagwankar <shubhu105@gmail.com>
2017-08-05 23:50:42 -0400

    8puzzle: Add twin(), neighbors(), tests.

--- 8puzzle/Board.java
+++ 8puzzle/Board.java
@@ -1,15 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class Board {
     private int moves;
     private String repr;
 
     private final int n;
+    private final int nn;
     private final int[][] blocks;
 
     public Board(int[][] blocks) {
         this.moves = 0;
         this.n = blocks.length;
+        this.nn = n*n;
         this.blocks = new int[n][n];
         for (int row = 0; row < n; row++)
             for (int col = 0; col < n; col++)
@@ -21,7 +28,6 @@ public class Board {
     }
 
     public int hamming() {
-        int nn = n*n;
         int distance = moves;
 
         for (int row = 0; row < n; row++)
@@ -49,8 +55,6 @@ public class Board {
     }
 
     public boolean isGoal() {
-        int nn = n*n;
-
         for (int row = 0; row < n; row++)
             for (int col = 0; col < n; col++)
                 if (blocks[row][col] != (row*n + col + 1) % nn)
@@ -60,15 +64,102 @@ public class Board {
     }
 
     public Board twin() {
-        return null;
+        int block1 = StdRandom.uniform(nn);
+        int block2 = StdRandom.uniform(nn);
+
+        while (block2 == block1)
+            block2 = StdRandom.uniform(nn);
+
+        int i1 = block1 / n;
+        int i2 = block2 / n;
+        int j1 = block1 % n;
+        int j2 = block2 % n;
+        int[][] blocks = boardCopy();
+
+        blocks[i1][j1] = this.blocks[i2][j2];
+        blocks[i2][j2] = this.blocks[i1][j1];
+
+        return new Board(blocks);
     }
 
+    @Override
     public boolean equals(Object y) {
-        return false;
+        Board that = (Board) y;
+
+        return this.toString().equals(that.toString());
     }
 
     public Iterable<Board> neighbors() {
-        return null;
+        return new Neighbors();
+    }
+
+    private class Neighbors implements Iterable<Board> {
+        @Override
+        public Iterator<Board> iterator() {
+            return new NeighborIterator();
+        }
+    }
+
+    private class NeighborIterator implements Iterator<Board> {
+        private ArrayList<Board> neighbors;
+
+        public NeighborIterator() {
+            int zi = 0;
+            int zj = 0;
+
+            for (int row = 0; row < n; row++) {
+                boolean found = false;
+                for (int col = 0; col < n; col++) {
+                    if (blocks[row][col] == 0) {
+                        zi = row;
+                        zj = col;
+                        found = true;
+                    }
+                }
+                if (found) break;
+            }
+
+            neighbors = new ArrayList<>();
+            if (zi > 0) {
+                int[][] copy = boardCopy();
+                copy[zi][zj] = blocks[zi - 1][zj];
+                copy[zi - 1][zj] = 0;
+                neighbors.add(new Board(copy));
+            }
+            if (zi < n - 1) {
+                int[][] copy = boardCopy();
+                copy[zi][zj] = blocks[zi + 1][zj];
+                copy[zi + 1][zj] = 0;
+                neighbors.add(new Board(copy));
+            }
+            if (zj > 0) {
+                int[][] copy = boardCopy();
+                copy[zi][zj] = blocks[zi][zj - 1];
+                copy[zi][zj - 1] = 0;
+                neighbors.add(new Board(copy));
+            }
+            if (zj < n - 1) {
+                int[][] copy = boardCopy();
+                copy[zi][zj] = blocks[zi][zj + 1];
+                copy[zi][zj + 1] = 0;
+                neighbors.add(new Board(copy));
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return neighbors.size() != 0;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public Board next() {
+            return neighbors.remove(0);
+        }
     }
 
     public String toString() {
@@ -98,6 +189,15 @@ public class Board {
         return repr.toString();
     }
 
+    private int[][] boardCopy() {
+        int[][] copy = new int[n][];
+
+        for (int row = 0; row < n; row++)
+            copy[row] = Arrays.copyOf(blocks[row], n);
+
+        return copy;
+    }
+
     public static void main(String[] args) {
         int blocks[][] = {
             { 8, 1, 3 },
@@ -105,10 +205,22 @@ public class Board {
             { 7, 6, 5 },
         };
 
+        int blocks2[][] = {
+            { 8, 1, 3 },
+            { 4, 0, 2 },
+            { 7, 6, 2 },
+        };
+
         Board board = new Board(blocks);
+        Board board2 = new Board(blocks2);
         StdOut.print(board);
+        StdOut.print(board2);
         StdOut.println("hamming   = " + board.hamming());
         StdOut.println("manhattan = " + board.manhattan());
+        StdOut.println("boards equal? " + board.equals(board2));
+
+        for (Board neighbor : board.neighbors())
+            StdOut.println(neighbor);
     }
 
 }
