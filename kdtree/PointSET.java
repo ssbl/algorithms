71b76182db404ceb0146d6699d40a5e41746baab
Shubham Lagwankar <shubhu105@gmail.com>
2017-08-08 23:54:21 -0400

    kdtree: Initial commit.

---
+++ kdtree/PointSET.java
@@ -0,0 +1,45 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+public class PointSET {
+
+    public PointSET() {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public int size() {
+
+    }
+
+    public void insert(Point2D p) {
+        if (p == null)
+            throw new IllegalArgumentException("argument is null");
+    }
+
+    public boolean contains(Point2D p) {
+        if (p == null)
+            throw new IllegalArgumentException("argument is null");
+    }
+
+    public void draw() {
+
+    }
+
+    public Iterable<Point2D> range(RectHV rect) {
+        if (rect == null)
+            throw new IllegalArgumentException("argument is null");
+    }
+
+    public Point2D nearest(Point2D p) {
+        if (p == null)
+            throw new IllegalArgumentException("argument is null");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
