1c37ceb72ddc7f64015fa717230af2cf9cf08b7f
Shubham Lagwankar <shubhu105@gmail.com>
2017-08-09 22:15:45 -0400

    kdtree: PointSET untested version.

--- kdtree/PointSET.java
+++ kdtree/PointSET.java
@@ -1,42 +1,75 @@
+import java.util.ArrayList;
+import java.util.TreeSet;
+
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 
 public class PointSET {
+    private TreeSet<Point2D> points;
 
     public PointSET() {
-
+        this.points = new TreeSet<>();
     }
 
     public boolean isEmpty() {
-
+        return points.isEmpty();
     }
 
     public int size() {
-
+        return points.size();
     }
 
     public void insert(Point2D p) {
         if (p == null)
             throw new IllegalArgumentException("argument is null");
+
+        points.add(p);
     }
 
     public boolean contains(Point2D p) {
         if (p == null)
             throw new IllegalArgumentException("argument is null");
+
+        return points.contains(p);
     }
 
     public void draw() {
-
+        for (Point2D point : points) {
+            point.draw();
+        }
     }
 
     public Iterable<Point2D> range(RectHV rect) {
         if (rect == null)
             throw new IllegalArgumentException("argument is null");
+
+        ArrayList<Point2D> list = new ArrayList<>();
+
+        for (Point2D point : points) {
+            if (rect.contains(point)) list.add(point);
+        }
+
+        return list;
     }
 
     public Point2D nearest(Point2D p) {
         if (p == null)
             throw new IllegalArgumentException("argument is null");
+        if (points.isEmpty())
+            return null;
+
+        Point2D min = points.first();
+        double minDistance = p.distanceTo(min);
+
+        for (Point2D point : points) {
+            double distance = p.distanceTo(point);
+            if (distance < minDistance) {
+                min = point;
+                minDistance = distance;
+            }
+        }
+
+        return min;
     }
 
     public static void main(String[] args) {
